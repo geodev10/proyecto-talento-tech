3f1760139e40a2bdb7261742f14f8f456b8f62e4
geodev10 <jorge.lizcano@correounivalle.edu.co>
2025-09-23 17:42:22 -0500

    Actualizacion paginas contacto y nosotros

---
+++ js/swiper-script.js
@@ -0,0 +1,59 @@
+const swiperProductos = new Swiper(".swiper-productos", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    992: { slidesPerView: 4 },
+  },
+});
+
+const swiperTestimonios = new Swiper(".swiper-testimonios", {
+  slidesPerView: 1.5,
+  spaceBetween: 20,
+  loop: true,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: { el: ".swiper-pagination", clickable: true },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  breakpoints: {
+    768: { slidesPerView: 2 },
+    992: { slidesPerView: 4 },
+  },
+});
+
+const swiperTestimoniosMobile = new Swiper(".swiper-testimonios-mobile", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  pagination: { el: ".swiper-pagination", clickable: true },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
